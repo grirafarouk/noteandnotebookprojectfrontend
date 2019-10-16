74d7229df4b68803475fe4f63115e17f24e27e7c
Grira <farouk.grira@sesame.com.tn>
2019-10-16 11:05:27 +0100

    adding the last pages of communication between the users

--- src/app/interfacedialogue/interfacedialogue.component.ts
+++ src/app/interfacedialogue/interfacedialogue.component.ts
@@ -1,58 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import * as SockJS from 'sockjs-client'
+import * as Stomp from 'stompjs'
 @Component({
   selector: 'app-interfacedialogue',
   templateUrl: './interfacedialogue.component.html',
   styleUrls: ['./interfacedialogue.component.css']
 })
 export class InterfacedialogueComponent implements OnInit {
-  const Stomp = require('webstomp-client')
 
 
   ngOnInit() {
   }
   title = 'grokonez';
   description = 'Angular-WebSocket Demo';
- 
+
   greetings: string[] = [];
   disabled = true;
   name: string;
   private stompClient = null;
- 
+
   constructor() { }
- 
+
   setConnected(connected: boolean) {
     this.disabled = !connected;
- 
+
     if (connected) {
       this.greetings = [];
     }
   }
- 
-  connect() { 
+  connect() {
     const socket = new SockJS('http://localhost:8685/gkz-stomp-endpoint');
     this.stompClient = Stomp.over(socket);
- 
+
     const _this = this;
     this.stompClient.connect({}, function (frame) {
       _this.setConnected(true);
       console.log('Connected: ' + frame);
- 
+
       _this.stompClient.subscribe('/topic/hi', function (hello) {
         _this.showGreeting(JSON.parse(hello.body).greeting);
       });
     });
   }
- 
+
   disconnect() {
     if (this.stompClient != null) {
       this.stompClient.disconnect();
     }
- 
+
     this.setConnected(false);
     console.log('Disconnected!');
   }
- 
+
   sendName() {
     this.stompClient.send(
       '/gkz/hello',
@@ -60,7 +59,7 @@ export class InterfacedialogueComponent implements OnInit {
       JSON.stringify({ 'name': this.name })
     );
   }
- 
+
   showGreeting(message) {
     this.greetings.push(message);
   }
